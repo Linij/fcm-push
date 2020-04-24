25f83983ffc4ea8b0bb36712b2ccef7c15814455
linji <237525603@qq.com>
2020-04-24 09:48:34 +0800

    env

--- config/.AutoMigrate.go
+++ config/.AutoMigrate.go
@@ -1,12 +1,16 @@
 package main
 
+/**
+ *  僅用於初始化安裝 table 使用
+ */
+
 import (
-	"fmt"
+    "fmt"
 	"gcm-push/datasources"
 	"gcm-push/models"
 )
 
-func run() {
+func main() {
 	datasources.DB.DropTableIfExists(&models.PushDevice{}, &models.PushJob{}, &models.PushMessage{}, &models.PushTag{})
 	fmt.Println("刪除原有表成功")
 	datasources.DB.CreateTable(&models.PushDevice{}, &models.PushJob{}, &models.PushMessage{}, &models.PushTag{})

0e83bf16e7699a122ceee2b178fdf4a3c1167241
linji <237525603@qq.com>
2020-04-24 14:54:26 +0800

    add ignore file

--- _gitlabci/start.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-ProjecHtdocsDir=/home/htdocs/gopush
-SUPER_CONFIG_PATH=/super_config
-
-export GO111MODULE=on
-\cp -rf ${SUPER_CONFIG_PATH}/* /etc/supervisord.d/
-/sbin/sshd
-/usr/bin/supervisord
-/usr/sbin/crond -n -p
